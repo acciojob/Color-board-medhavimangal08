132c60e43d009b624e0bba751c3e1837046e4995
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 17:33:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let bigsquare=document.querySelector(".container")
+for(let i=0;i<800;i++){
+	let sq=document.createElement("div")
+	sq.className="square"
+	bigsquare.appendChild(sq)
+}

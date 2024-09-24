3236421f3d1d372dafe0d6a726fe4223f1d5d519
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-24 21:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let bigsquare=document.querySelector(".container")
 for(let i=0;i<800;i++){
 	let sq=document.createElement("div")
 	sq.className="square"
+	sq.style.backgroundColor='rgb(29, 29, 29)';
 	bigsquare.appendChild(sq)
 }
